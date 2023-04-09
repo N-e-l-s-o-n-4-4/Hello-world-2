2baf090e5b4707e02a0bb7548e38298075d0e798
N-e-l-s-o-n-4-4 <Nelson44ne@gmail.com>
2023-04-09 19:29:52 +0300

    lesson 5

---
+++ if else elif lesson5.py
@@ -0,0 +1,27 @@
+num = 4
+if num == 1:
+    print("one")
+else:
+    if num == 2:
+        print("two")
+    else:
+        if num == 3:
+            print("Three")
+        else:
+            print("Somthing else")
+
+num = 3
+if num == 1:
+    print("one")
+elif num == 2:
+    print("two")
+elif num == 3:
+    print("Three")
+else:
+    print("Somthing else")
+
+age = int(input())
+if age >= 18:
+    print("Allowed")
+else:
+    print("Sorry")
