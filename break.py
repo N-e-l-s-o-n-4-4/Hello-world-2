5d18bfcc216b02f2eed59caf1938b357a4b3b2a2
N-e-l-s-o-n-4-4 <Nelson44ne@gmail.com>
2023-04-12 22:36:21 +0300

    lesson 6

--- break.py
+++ break.py
@@ -1,8 +1,43 @@
-found= None # Значение *Ничего*
-for i in "hello":
-    if i =="l":
-        found = True
-        break # оператор выхода из цикла
+# found= None # Значение *Ничего*
+# for i in "hello":
+#        found = True
+#        break # оператор выхода из цикла
+# else:
+#    found = False
+# print(found)
+
+
+# i=5
+# while True:
+#   print(i)
+#   i=i-1
+#   if i<=1:
+#       break
+
+
+#total = 0
+#i = 0
+#while i < 5:
+#    age = int(input())
+ #   i = i + 1
+  #  if age < 3:
+   #     continue
+    #total += 100
+#print(total)
+
+#i=0
+#x=0
+#while i <4:
+#    x+=i
+#    i+=1
+#    print(x)
+
+
+x=int(input())
+if x>5:
+    if x<8:
+        print(x+1)
+    else:
+        print(x-1)
 else:
-    found = False
-print(found)
+    print(x)
